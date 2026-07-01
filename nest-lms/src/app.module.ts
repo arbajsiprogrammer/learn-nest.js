67c665ebbd214023ba4821f1f279d3b3f2bfb3cb
arbajsiprogrammer <arbajsiprogrammer@gmail.com>
2026-07-01 10:55:04 +0530

    fix: update @types/node to version 24.13.2 in package.json and package-lock.json

--- nest-lms/src/app.module.ts
+++ nest-lms/src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
-import { UserService } from './user/user.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
